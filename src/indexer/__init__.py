9b1185352a6e19552812773971df8f17794bf804
Krzyzyk33 <piotr.hyzyk.bb@gmail.com>
2026-07-14 21:38:41 +0200

    Delete src directory

--- src/indexer/__init__.py
+++
@@ -1,5 +0,0 @@
-from .symbol_db import SymbolDB
-from .ts_parser import parse_file, Symbol
-from .scan import full_or_incremental_scan
-
-__all__ = ["SymbolDB", "parse_file", "Symbol", "full_or_incremental_scan"]
